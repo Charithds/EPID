b0e32b36af156afbf645bb3e4db8b4301f8f5375
Charith Soysa <charith.14@cse.mrt.ac.lk>
2016-12-14 02:42:27 +0530

    Just some minor changes
    Havent upgraded to the correct database still

---
+++ Server/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('role:owner');
+    }
+
+    public function index()
+    {
+        return view('owner.users.home');
+    }
+}
